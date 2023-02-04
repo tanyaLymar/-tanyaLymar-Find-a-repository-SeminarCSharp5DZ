33d7b244886b79616e89d15fa348e0d7aa370c78
Tatiana Lymar <tanya.lymar87@gmail.com>
2023-02-05 00:09:45 +0300

    Task2 solved.

---
+++ task2/Program.cs
@@ -0,0 +1,33 @@
+﻿// Задать двумерный массив следующим правилом: Aₘₙ = m+n
+
+void FillMas (int[,]array)
+{
+   for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            array[i,j] = i + j;
+        }
+    }
+}
+
+void PrintMas(int [,] array)
+{for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+           Console.Write($"{array[i,j]}  ");
+        }
+        Console.WriteLine();
+    }
+}
+
+
+    
+Console.Write("Введите количество строк массива: ");    
+int m = int.Parse(Console.ReadLine() ?? "0");
+Console.Write("Введите количество столбцов массива: ");    
+int n = int.Parse(Console.ReadLine() ?? "0");
+int [,] array = new int[m, n];
+FillMas(array);
+PrintMas(array);
